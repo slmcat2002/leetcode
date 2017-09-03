d99fcb9e43a9bab84cd4988fd8e5d57797ca90cb
slmcat2002 <slmcat2002@hotmail.com>
2017-09-03 22:08:13 +0800

    23 pq

---
+++ src/main/java/_23_Merge_k_Sorted_Lists/Solution.java
@@ -0,0 +1,30 @@
+package _23_Merge_k_Sorted_Lists;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        return null;
+    }
+}
