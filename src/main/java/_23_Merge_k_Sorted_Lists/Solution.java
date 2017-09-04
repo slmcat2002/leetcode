9eb2a6652de60c9d1610d77ac40e4f18d0065bc0
slmcat2002 <slmcat2002@hotmail.com>
2017-09-04 09:50:39 +0800

    23

--- src/main/java/_23_Merge_k_Sorted_Lists/Solution.java
+++ src/main/java/_23_Merge_k_Sorted_Lists/Solution.java
@@ -11,20 +11,33 @@ import java.util.PriorityQueue;
  *     ListNode(int x) { val = x; }
  * }
  */
+
 class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
 }
-
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
             @Override
-            public int compare(ListNode o1, ListNode o2) {
-                return o1.val - o2.val;
+            public int compare(ListNode n1, ListNode n2){
+                return Integer.compare(n1.val, n2.val);
             }
         });
-        return null;
+        for (ListNode n : lists){
+            if (n != null) pq.add(n);
+        }
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        while(pq.size() > 0){
+            ListNode node = pq.poll();
+            current.next = node;
+            current = node;
+            if (node.next != null){
+                pq.add(node.next);
+            }
+        }
+        return head.next;
     }
 }
